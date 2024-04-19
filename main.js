cd20fe5e88a2b12b91a64ab8d322687c6c279bd1
t-coffe <tcoffe@edenschool.fr>
2024-04-19 11:02:14 +0200

    work completed

--- main.js
+++ main.js
@@ -6,32 +6,29 @@ const ranger = document.querySelector('input');
 const start = document.getElementById('start');
 const check = document.getElementById('check');
 
-const win = 0;
+let win = 0;
 score.textContent = win;
-const stock = 0;
+let stock = 0;
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-start.addEventListener('click', ()=> {
-  stock = parseInt(getRandomInt(100));
-  console.log(stock);
-  random.textContent = stock;
-  console.log(random);
-})
+stock = parseInt(getRandomInt(100));
+random.textContent = stock;
 
 
 check.addEventListener('click', ()=> {
-  let compare = parseInt(random.textContent);
-  console.log(compare);
   if (1 === ranger.value) {
     win += 200;
-  } else if (compare - ranger.value < 5 && compare - ranger.value > -5) {
+  } else if (stock - ranger.value < 5 && stock - ranger.value > -5) {
     win += 100;
   } else {
     alert('too bad');
   }
+  score.textContent = win;
+  stock = parseInt(getRandomInt(100));
+  random.textContent = stock;
 })
 
 
